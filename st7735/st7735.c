6e38581e20ad3ea353e70f48b1ad7578364b2e9d
WangLaoEr <suguguan@qq.com>
2023-07-09 03:43:40 +0800

    Fixed inconsistency between function declaration and definition

--- st7735/st7735.c
+++ st7735/st7735.c
@@ -303,10 +303,10 @@ void ST7735_InvertColors(bool invert) {
     ST7735_Unselect();
 }
 
-void ST7735_SetGamma(uint8_t gamma)
+void ST7735_SetGamma(GammaDef gamma)
 {
 	ST7735_Select();
 	ST7735_WriteCommand(ST7735_GAMSET);
-	ST7735_WriteData(&gamma, sizeof(gamma));
+	ST7735_WriteData((uint8_t *) &gamma, sizeof(gamma));
 	ST7735_Unselect();
 }
